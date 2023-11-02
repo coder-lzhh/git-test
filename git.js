448dee8b67d477123d6322a4c1adeb1d5eba0594
zhihao.liang <zhihao1.liang@ly.com>
2023-11-02 20:49:52 +0800

    add

--- git.js
+++ git.js
@@ -1,9 +1,8 @@
-console.log('1111111')
-console.log('2222222')
-console.log('3333333')
-console.log('6666666')
+console.log("1111111");
+console.log("2222222");
+console.log("3333333");
+console.log("6666666");
 
-
-
-console.log(77777777)
-console.log('+++++++')
+console.log(77777777);
+console.log(77777777);
+console.log("+++++++");

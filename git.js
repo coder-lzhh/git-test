cd81491e5e4ebc55f474811c2b55b474260dd613
zhihao.liang <zhihao1.liang@ly.com>
2023-11-02 20:50:24 +0800

    rebase

--- git.js
+++ git.js
@@ -1,9 +1,7 @@
-console.log('1111111')
-console.log('2222222')
-console.log('3333333')
-console.log('6666666')
+console.log("1111111");
+console.log("2222222");
+console.log("3333333");
+console.log("6666666");
 
-
-
-console.log(77777777)
-console.log('+++++++')
+console.log("rebase");
+console.log("+++++++");
